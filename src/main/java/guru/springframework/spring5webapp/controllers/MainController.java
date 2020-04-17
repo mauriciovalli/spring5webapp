9c7204f9e23244b8cb93c3fb9ebe71b38d431670
Mauricio Valli <mauricio.valli@bpmx.com.ar>
2020-04-17 17:41:20 -0300

    Add Hello msg

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/MainController.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
